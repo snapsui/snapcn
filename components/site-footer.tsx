11c403889e2d5fecf984194eda54e9cbff326571
jeet1desai <jeetdesai995@gmail.com>
2024-09-17 21:27:46 +0530

    update the footer

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -13,14 +13,14 @@ export function SiteFooter() {
       <div className="container flex flex-col items-center justify-between gap-4 md:h-16 md:flex-row">
         <p className="text-center text-sm leading-loose text-muted-foreground md:text-left">
           Brought to you by{" "}
-          <a
+          {/* <a
             href={siteConfig.links.twitter}
             target="_blank"
             rel="noreferrer"
             className="font-medium underline underline-offset-4"
           >
             dillion
-          </a>
+          </a> */}
           .
         </p>
       </div>
